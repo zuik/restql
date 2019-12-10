162a56f7ab77bd68d5d23cec20ce3851c86bf579
Zui <zuikng@gmail.com>
2019-12-09 22:01:39 -0500

    Run model.py to init db

--- restql/model.py
+++ restql/model.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     
     engine = create_engine('sqlite:///test.db', echo=True)
-    # Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
