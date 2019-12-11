c3fbc66572f2ad632f7f24bad6d19580389efb0d
Zui <zuikng@gmail.com>
2019-12-11 18:37:44 -0500

    Filter working

--- js/restql.js
+++ js/restql.js
@@ -3,20 +3,30 @@ import axios from 'axios';
 class Query {
     constructor(model) {
         this.model = model;
+        this.filterCriteria = undefined;
     }
 
     async all() {
-        // GET model/all
-        console.log(`GET ${this.model}/all`);
-        const url = `${this.model}/all`;
-
-        const r = await axios.get(url);
-        if (r.data) {
-            return r.data;
-        } else {
-            return null;
+        const url = `${this.model}`;
+        // Right now we only support all() to finalize the query.
+        let params = {
+            "finalize": "all"
+        };
+        if (this.filterCriteria) {
+            params["filter"] = this.filterCriteria;
         }
+        console.log(url, params);
+        const {data} = await axios.get(url, {params: params});
 
+        return data;
+
+    }
+
+    filter(criteria) {
+        this.filterCriteria = criteria;
+        this.model = `${this.model}/filter`;
+
+        return this;
     }
 }
 
@@ -30,9 +40,12 @@ function query(model) {
 
 async function main() {
     let q = new Query("http://localhost:5000/users");
-    let result = await q.all();
-
+    // const result = await q.all();
+    const result = await q.filter({"name": {"eq": "ed"}}).all();
     console.log(result);
+    result.map((v) => {
+        console.log(v.fullname);
+    });
 }
 
 if (require.main === module) {
--- restql/view.py
+++ restql/view.py
@@ -1,7 +1,7 @@
 import json
 
 import sqlalchemy
-from flask import Flask, Blueprint, g, jsonify
+from flask import Flask, request, g, jsonify, abort
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from sqlalchemy.orm import sessionmaker
@@ -35,12 +35,32 @@ def create_endpoint(model):
     table = model.__table__  # type: sqlalchemy.schema.Table
     table_name = table.name
 
-    @app.route(f"/{table_name}/all")
+    @app.route(f"/{table_name}")
     def select_all():
         session = get_session()
-        r = session.query(model).all()
-        return jsonify(r)
+        finalize = request.args.get("finalize", "all")
+        if finalize == "all":
+            r = session.query(model).all()
+            return jsonify(r)
+        else:
+            return abort(400)
+    @app.route(f"/{table_name}/filter")
+    def filter():
+        session = get_session()
+        q = session.query(model)
 
+        filter = json.loads(request.args.get("filter"))
+        print(filter)
+        if filter:
+            for k, v in filter.items():
+                if "eq" in v.keys():
+                 q = q.filter(getattr(model, k) == v["eq"])
+
+        finalize = request.args.get("finalize", "all")
+        if finalize != "all":
+            return abort(400)
+        else:
+            return jsonify(q.all())
     return app
 
 
